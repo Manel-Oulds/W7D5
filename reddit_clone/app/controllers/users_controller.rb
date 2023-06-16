6b040164cb0d65e0d70d6f8517bf57790293e9b7
jl2kk <jetpoke1011@gmail.com>
2023-06-16 11:27:05 -0400

    created controllers

--- reddit_clone/app/controllers/users_controller.rb
+++ reddit_clone/app/controllers/users_controller.rb
@@ -1,2 +1,37 @@
 class UsersController < ApplicationController
+    before_action :require_logged_in, only: [:show, :index]
+    before_action :require_logged_out, only: [:create, :new]
+
+    def index 
+        @users = User.all 
+    end 
+
+    def new
+        @user = User.new 
+    end 
+
+    def create 
+        @user = User.new(params_users)
+        if @user.save 
+            login(@user)
+            redirect_to user_url(@user)
+        else 
+            flash.now[:errors] = @user.errors.full_messages
+            render :new
+        end
+    end 
+
+    def show 
+        @user = User.find(params[:id])
+        render :show
+    end 
+
+
+    private 
+
+    def params_users
+        params.require(:user).permit(:username, :password)
+    end 
+
+
 end
